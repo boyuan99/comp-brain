945df805ac1ae5a39960b8084654493eb88329c3
Bo Yuan <93609557+boyuan99@users.noreply.github.com>
2022-09-22 16:36:41 -0400

    update neurons
    add IAF and LIF neurons, update comments for morris-lecar model.

--- compbrain/neurons/integrate_and_fire.py
+++ compbrain/neurons/integrate_and_fire.py
@@ -3,26 +3,24 @@ from collections import OrderedDict
 from compbrain.core import BaseComponent, CompBrainModelError
 
 
-class MorrisLecarNeuron(BaseComponent):
-    """Morris Lecar Neuron Model"""
+class IAFNeuron(BaseComponent):
+    """Integrate and Fire Neuron Model"""
 
     def __init__(self, name, **kwargs):
         """
         initial function
-        :param name: name for the morris lecar neuron, every neuron in the circuit
+        :param name: name for the integrate and fire neuron, every neuron in the circuit
             should be different
         :param kwargs: keyword arguments that overwrite initial conditions of state
             variables and values of parameters
         """
-        super(MorrisLecarNeuron, self).__init__(name, **kwargs)
+        super(IAFNeuron, self).__init__(name, **kwargs)
         self.params: OrderedDict = OrderedDict(
-            V_1=-15.0, V_2=2.0, V_3=-45.0, V_4=0.8,
-            phi=0.0005, C=1, dt=1e-4, offset=50,
-            E_L=-50.0, E_Ca=120.0, E_K=-75.0,
-            g_L=0.1, g_Ca=2.0, g_K=7.0,
+            V_T=-50.0, V_0=-80.0, V_imp=-20,
+            C=1.0, dt=1e-5,
         )
         self.states: OrderedDict = OrderedDict(
-            V=[-44.5], N=[0.5]
+            V=[-80],
         )
 
         if 'params' in kwargs.keys():
@@ -61,17 +59,16 @@ class MorrisLecarNeuron(BaseComponent):
 
     def reset_value(self):
         """
-        reset the morris lecar neuron to its initial values
+        reset the integrate and fire neuron to its initial values
         """
         V_init = self.states['V'][0]
-        N_init = self.states['N'][0]
         self.states = OrderedDict(
-            V=[V_init], N=[N_init]
+            V=[V_init]
         )
 
     def compute(self, I_syn: float, I_ext: float, dt:float) -> dict:
         """
-        Morris-Lecar gradient function
+        Integrate and Fire gradient function
 
         :param I_syn: the input synapse current
         :param I_ext: the external injection current
@@ -79,35 +76,41 @@ class MorrisLecarNeuron(BaseComponent):
         :return: dict(V, N)
         """
         V = self.states['V'][-1]
-        N = self.states['N'][-1]
-        if N < 1e-7:
-            N = 0
-        elif N > 1:
-            N = 1
 
-        dt = dt*1e3
-        V_1 = self.params['V_1']
-        V_2 = self.params['V_2']
-        V_3 = self.params['V_3']
-        V_4 = self.params['V_4']
-        phi = self.params['phi']
-        offset = self.params['offset']
-        E_L = self.params['E_L']
-        E_Ca = self.params['E_Ca']
-        E_K = self.params['E_K']
-
-        g_L = self.params['g_L']
-        g_Ca = self.params['g_Ca']
-        g_K = self.params['g_K']
+        dt = dt * 1000
+        V_T = self.params['V_T']
+        V_0 = self.params['V_0']
+        V_imp = self.params['V_imp']
         C = self.params['C']
 
-        dV = (I_ext + offset - I_syn - g_L * (V - E_L) - 0.5 * g_Ca * (1 + np.tanh((V - V_1) / V_2)) * (V - E_Ca) - g_K * N * (
-                    V - E_K)) / C
-        dN = (0.5 * (1 + np.tanh((V - V_3) / V_4)) - N) * (phi * np.cosh((V - V_3) / (2 * V_4)))
+        dV = I_ext/C
 
         V1 = V + dV * dt
-        N1 = N + dN * dt
+        if V1 >= V_T:
+            if V1 >= V_imp:
+                V1 = V_0
+            else:
+                V1 = V_imp
 
         self.states['V'].append(V1)
-        self.states['N'].append(N1)
         return self.states
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    neuron = IAFNeuron('L1')
+    dt = 1e-5
+    t = np.arange(0, 1, dt)
+
+    I_ext = np.zeros_like(t)
+    I_ext[int(0.2 * len(t)):int(0.8 * len(t))] = 1
+
+    V = np.zeros_like(t)
+    V[0] = 0.0
+
+    for i in range(len(t)):
+        if i < len(t)-1:
+            neuron.compute(I_ext=I_ext[i], I_syn=0, dt=dt)
+
+    plt.plot(t, neuron.states['V'])
+    plt.show()
